a4ba1f62da9c6afbf3cbf81c041d0e70cfdb1034
Atilon dev <129638566+pelapato@users.noreply.github.com>
2024-08-10 20:22:01 -0300

    portifolio original

---
+++ portifolio.dev/front-end/dev40.js
@@ -0,0 +1,35 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal(".txt-layout",{
+    duration:400,
+    distance:"60px"
+})
+revelar.reveal(".txt-card",{
+    duration:800,
+    distance:"60px"
+})
+
+const menu = document.querySelector(".text_menu")
+const nav = document.querySelector("nav")
+var verify = 1
+const cima = document.createElement("strong")
+cima.setAttribute('id','Rolagem_Topo')
+const varX = document.createElement("a")
+varX.setAttribute('href','#logo')
+const imgRolagem_Topo =document.createElement('img')
+imgRolagem_Topo.setAttribute('src','./src/imagens/chevron-up.svg')
+document.querySelector("body").appendChild(cima)
+cima.appendChild(varX)
+varX.appendChild(imgRolagem_Topo)
+
+menu.addEventListener('click',()=>{
+    ++verify
+    if(verify%2==0){
+        nav.classList.add("menu_visibility")
+        document.querySelector("main").addEventListener('click',()=>{
+            nav.classList.remove('menu_visibility')
+        })
+    }else{
+        nav.classList.remove("menu_visibility")
+    }
+})
