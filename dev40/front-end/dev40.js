d804f5c7bdad518a6e5aa8025a7de57b141f5b02
Atilon.dev40 <129638566+pelapato@users.noreply.github.com>
2024-12-17 00:32:23 +0000

    sistema de menu v1

--- dev40/front-end/dev40.js
+++ dev40/front-end/dev40.js
@@ -1,3 +1,23 @@
 const escrever = document.querySelector(".escrever");
 const technologias = ["Web Designer","Game","Dev"]
 console.log(escrever);
+const menu = document.querySelector("#menu");
+const menu_linha = document.querySelector("#linha");
+const nav = document.querySelector("nav");
+
+var sistema_de_menu_verify = 1;
+
+const sistema_de_menu=()=>{
+    menu.addEventListener('click',()=>{
+        menu_linha.classList.toggle("lin");
+
+        sistema_de_menu_verify++
+        let verify_cont =(sistema_de_menu_verify%2==0) ? true : false;  
+        if(verify_cont){
+            nav.classList.add("menu_ativo");
+        }else{
+            nav.classList.remove("menu_ativo");
+        }
+    })
+}
+sistema_de_menu()
